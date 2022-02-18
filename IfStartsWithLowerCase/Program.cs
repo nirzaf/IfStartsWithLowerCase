dcee89cf847d00300c557801ed575aeaa102a38b
Farook M Fazrin <nirzaf1@hotmail.com>
2022-02-18 22:46:44 +0530

    Add project files.

---
+++ IfStartsWithLowerCase/Program.cs
@@ -0,0 +1,23 @@
+﻿//Given a string, write a method that checks if each word in the string starts with lower case and if so, removes this letter from the string.
+//Expected input and output
+//IfStartsWithLowerCase("Alfa Beta gamma") → "Alfa Beta amma"
+
+global using static System.Console;
+
+WriteLine(IfStartsWithLowerCase("tthis iis ffake ssentence."));
+WriteLine(IfStartsWithLowerCase("Praesent vitae convallis purus."));
+
+string IfStartsWithLowerCase(string word)
+{
+    string[] words = word.Split(' ');
+
+        for (int i=0;i< words.Length;i++)
+        {
+            if (char.IsLower(words[i][0]))
+            {
+                words[i] = words[i].Substring(1);
+            }
+        }
+        
+    return string.Join(" ", words);
+}
